127e8dabd2eee381bedcf1829c986295cebf2b6e
ranizine <yaroslav76071@mail.ru>
2023-11-05 23:06:29 +0300

    first commit

---
+++ timer.js
@@ -0,0 +1,27 @@
+const timerElement = document.getElementById('timer');
+let timeLeft = 5 * 60;
+let timerInterval;
+
+const saveDrawing = () => {
+    const link = document.createElement("a");
+    link.download = `${Date.now()}.jpg`;
+    link.href = canvas.toDataURL();
+    link.click();
+}
+
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    
+    if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        saveDrawing();
+    }
+    timeLeft--;
+}
+
+window.addEventListener('load', () => {
+    updateTimer();
+    timerInterval = setInterval(updateTimer, 1000);
+});
